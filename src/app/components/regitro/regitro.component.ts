8d0b1e57adc493ed81e7fd77106e8e742c703673
wandy141 <115728722+wandy141@users.noreply.github.com>
2023-05-06 20:19:18 -0600

    spinner en boton de carga

--- src/app/components/regitro/regitro.component.ts
+++ src/app/components/regitro/regitro.component.ts
@@ -15,27 +15,21 @@ export class RegitroComponent {
   password: any = '';
   accediendo: boolean = false;
   hayerrores: boolean = false;
-  letrero: string = 'acceder';
 
-  // el contructor se pone prrivada para que funcione dentro de esta pagina y el htp para la conexion con laravel
-  // {route es para el cambio de pagina}
+  
   constructor( public router: Router, public servicio: ApiDBService) {
-    console.log('Login');
   }
 
   mesajeError() {
-    //para no madarle mas de una peticion al momento de oprimir el boto varias veces
     if (this.accediendo) {
       return;
     }
 
-    //declarando la variables  para usarlas como mensaje
     this.msgUsuario = false;
     this.msgPassword = false;
     this.accediendo = false;
     this.hayerrores = false;
 
-    //si el usuario(el input) esta vacio la variable del donde esta en mensaje va a presentar
     if (this.usuario == '') {
       this.msgUsuario = true;
       setTimeout(() => {
@@ -44,7 +38,6 @@ export class RegitroComponent {
       this.hayerrores = true;
     }
 
-    //si el contraseña(el input) esta vacio la variable del donde esta en mensaje va a presentar
     if (this.password == '') {
       this.msgPassword = true;
       setTimeout(() => {
@@ -53,22 +46,14 @@ export class RegitroComponent {
       this.hayerrores = true;
     }
     //esto es para si hay algun error el boton no cambie o recargue
-    //la variable esta definida falso pero si un error se ejecuta va a convertirse true
-    //y se va a devolver antes de que se ejecute el boton
     if (this.hayerrores) {
       return;
     }
     this.accediendo = true;
-    this.letrero = 'accediendo';
-    //set timeout es para que el tiempo de carga dure mas
     setTimeout(() => {
-      // a la url de la api le mandamos le hacemos la peticion de lo queremos
     
         this.servicio.login(this.usuario,this.password).subscribe((retorno) => {
-          console.log(retorno);
           this.accediendo = false;
-          this.letrero = 'acceder';
-          //aqui si el retorno que viene siendo la peticion a la api sal correcta te a mandar a otra pagina
           if (retorno.resultado == true) {
             this.servicio.token = retorno.token;
             localStorage.setItem('token', retorno.token);
