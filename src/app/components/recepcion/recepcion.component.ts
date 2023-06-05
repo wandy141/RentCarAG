f961da5433bdb0355549868af4e3fe228bf85bed
wandy141 <115728722+wandy141@users.noreply.github.com>
2023-06-04 20:57:06 -0600

    nada importante

--- src/app/components/recepcion/recepcion.component.ts
+++ src/app/components/recepcion/recepcion.component.ts
@@ -15,4 +15,37 @@ constructor(private servicio: ApiDBService){
   this.data = this.servicio.getData();
 }
 
+
+
+
+getDescripcionTipo(tipo: number) {
+  let retorno: string = 'tipo';
+  switch (tipo) {
+    case 1:
+      retorno = 'Economico';
+      break;
+
+    case 2:
+      retorno = 'Compacto';
+      break;
+
+    case 3:
+      retorno = 'Tamaño Normal';
+      break;
+
+    case 4:
+      retorno = 'Premium';
+      break;
+
+    case 5:
+      retorno = 'Lujo';
+      break;
+
+    case 6:
+      retorno = 'Camion';
+      break;
+  }
+  return retorno;
+}
+
 }
