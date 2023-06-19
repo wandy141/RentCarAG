cec3c97cce48ec230afa265a4b60e10490c6fdd3
Almer123 <2190002@ipisa.edu.do>
2023-06-19 11:21:37 -0400

    arreglar api

--- src/app/services/api-db.service.ts
+++ src/app/services/api-db.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { vehiculo } from '../clasebd/vehiculo';
-import { Observable, map } from 'rxjs';
+import {  map } from 'rxjs';
 import { Observable, Subject } from 'rxjs';
 import { tipoVehiculo } from '../clasebd/tipoVehiculo';
 import { usuarios } from '../clasebd/usuarios';
