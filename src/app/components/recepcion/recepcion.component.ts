4e93706a4af2b38aa952d0d0d506107848a8993c
ldkcrixx <2190012@ipisa.edu.do>
2023-06-19 14:34:27 -0400

    Completo el formulario de pago

--- src/app/components/recepcion/recepcion.component.ts
+++ src/app/components/recepcion/recepcion.component.ts
@@ -12,58 +12,59 @@ import Swal from 'sweetalert2';
   templateUrl: './recepcion.component.html',
   styleUrls: ['./recepcion.component.css']
 })
-export class RecepcionComponent implements OnInit{
+export class RecepcionComponent implements OnInit {
   data: any;
-  listaVehiculos:Array<vehiculo> = [];
+  listaVehiculos: Array<vehiculo> = [];
   // idalquiler: number= 0;
-  usuario: string='';
+  usuario: string = '';
   // idcliente: number= 0;
   // nombrecliente: string='';
-  fecha: string='';
+  fecha: string = '';
   // idvehiculo: number= 0;
   // precio: number= 0;
-  fechaini:string='';
-  fechafin:string='';
-  dias: number=0 ;
-  total: number= 0;
-  nombre: string='';
-  correo:string='';
-  cedula: string='';
-  direccion: string='';
-  nacionalidad:string='';
-  lugardeentrega:string='';
-  lugardedevolucion:string='';
+  fechaini: string = '';
+  fechafin: string = '';
+  dias: number = 0;
+  total: number = 0;
+  nombre: string = '';
+  correo: string = '';
+  cedula: string = '';
+  direccion: string = '';
+  nacionalidad: string = '';
+  lugardeentrega: string = '';
+  lugardedevolucion: string = '';
   diferenciaDias: number = 0;
   nationalities: Array<string> = [];
   selectedNacionalidad: string = '';
   filteredNacionalidades: Array<string> = [];
-  seguroValue:number = 20;
+  seguroValue: number = 20;
   seguroSi: string = 'normal';
   seleccionadoSi: boolean = false;
   seleccionadoNo: boolean = false;
   codigocv: number = 0;
   ano: number = 0;
-   mes: string = ''; 
-   numerodetarjeta: number = 0;
-   nombretj: string = '';
-years: number[] = [];
-telefono:string = '';
+  mes: string = '';
+  numerodetarjeta: number = 0;
+  nombretj: string = '';
+  years: number[] = [];
+  telefono: string = '';
+  idcli: number = 0;
 
 
-months: string[] = [
-  'Enero',
-  'Febrero',
-  'Marzo',
-  'Abril',
-  'Mayo',
-  'Junio',
-  'Julio',
-  'Agosto',
-  'Septiembre',
-  'Octubre',
-  'Noviembre',
-  'Diciembre'
-];
+  months: string[] = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+  ];
 
 
 
@@ -76,169 +77,170 @@ months: string[] = [
   }
 
 
-  
+
 
   getTotal() {
     return this.total + this.seguroValue;
   }
 
-constructor(private servicio: ApiDBService, private router: Router){
-  this.data = this.servicio.getData();
+  constructor(private servicio: ApiDBService, private router: Router) {
+    this.data = this.servicio.getData();
 
 
-  const currentYear = new Date().getFullYear();
-  const startYear = currentYear - 10; // Puedes ajustar el rango según tus necesidades
+    const currentYear = new Date().getFullYear();
+    const startYear = currentYear - 10; // Puedes ajustar el rango según tus necesidades
 
-  for (let year = startYear; year <= currentYear; year++) {
-    this.years.push(year);
+    for (let year = startYear; year <= currentYear; year++) {
+      this.years.push(year);
+    }
   }
-}
 
-ngOnInit(): void {
- const fechas = this.servicio.getInputValue();
- this.fechaini = fechas.fechaIni;
- this.fechafin = fechas.fechaFin;
- this.lugardeentrega = fechas.entrega;
- this.lugardedevolucion = fechas.devolucion;
+  ngOnInit(): void {
+    const fechas = this.servicio.getInputValue();
+    this.fechaini = fechas.fechaIni;
+    this.fechafin = fechas.fechaFin;
+    this.lugardeentrega = fechas.entrega;
+    this.lugardedevolucion = fechas.devolucion;
 
- this.servicio.getNombreUser().subscribe((nombre) => {
-  this.usuario = nombre;
-});
+    this.servicio.getNombreUser().subscribe((nombre) => {
+      this.usuario = nombre;
+    });
 
-const today = new Date();
-const yyyy = today.getFullYear();
-const mm = String(today.getMonth() + 1).padStart(2, '0');
-const dd = String(today.getDate()).padStart(2, '0');
-const hora = String(today.getHours()).padStart(2, '0');
-const minutos = String(today.getMinutes()).padStart(2, '0');
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const dd = String(today.getDate()).padStart(2, '0');
+    const hora = String(today.getHours()).padStart(2, '0');
+    const minutos = String(today.getMinutes()).padStart(2, '0');
 
-this.fecha = `${yyyy}-${mm}-${dd} ${hora}:${minutos}`;
-this.dias = this.servicio.getDias();
+    this.fecha = `${yyyy}-${mm}-${dd} ${hora}:${minutos}`;
+    this.dias = this.servicio.getDias();
 
-this.nationality();
-this.filteredNacionalidades = this.nationalities;
-this.calcular();
+    this.nationality();
+    this.filteredNacionalidades = this.nationalities;
+    this.calcular();
 
-}
+  }
 
-alerta:boolean = false;
+  alerta: boolean = false;
 
-entrarAlquiler() {
- setTimeout(() => {
-  
-  
-const idalquiler = 0;
-  let alquilerTemp: alquiler = new alquiler();
-  alquilerTemp.idalquiler = idalquiler;
-  alquilerTemp.usuario = this.usuario;
-  alquilerTemp.fecha = this.fecha;
-  alquilerTemp.idcliente = this.idcli;
-  alquilerTemp.nombrecliente = this.nombre;
-  alquilerTemp.idvehiculo = this.data.idvehiculo;
-  alquilerTemp.seguro = this.seguroSi;
-  alquilerTemp.precio = this.data.precio;
-  alquilerTemp.fechaini = this.fechaini;
-  alquilerTemp.fechafin = this.fechafin;
-  alquilerTemp.dias = this.dias;
-  alquilerTemp.total = this.total;
-  alquilerTemp.lugar_entrega = this.lugardeentrega;
-  alquilerTemp.lugar_recibir = this.lugardedevolucion;
-
-
-
-
-  this.servicio.insertarAlquiler(alquilerTemp).subscribe((resultado) => {
-    if (resultado) {
-      this.limpiar();
-
-      this.alerta=true;
-      setTimeout(() => {
-      this.alerta=false;
-      }, 3000);
+  entrarAlquiler() {
+    console.log(this.idcli);
     
+    setTimeout(() => {
+
+
+      const idalquiler = 0;
+      let alquilerTemp: alquiler = new alquiler();
+      alquilerTemp.idalquiler = idalquiler;
+      alquilerTemp.usuario = this.usuario;
+      alquilerTemp.fecha = this.fecha;
+      alquilerTemp.idcliente = this.idcli;
+      alquilerTemp.nombrecliente = this.nombre;
+      alquilerTemp.idvehiculo = this.data.idvehiculo;
+      alquilerTemp.seguro = this.seguroSi;
+      alquilerTemp.precio = this.data.precio;
+      alquilerTemp.fechaini = this.fechaini;
+      alquilerTemp.fechafin = this.fechafin;
+      alquilerTemp.dias = this.dias;
+      alquilerTemp.total = this.total;
+      alquilerTemp.lugar_entrega = this.lugardeentrega;
+      alquilerTemp.lugar_recibir = this.lugardedevolucion;
+
+
+
+
+      this.servicio.insertarAlquiler(alquilerTemp).subscribe((resultado) => {
+        if (resultado) {
+          this.limpiar();
+
+          this.alerta = true;
+          setTimeout(() => {
+            this.alerta = false;
+          }, 3000);
+
+        }
+      });
+    }, 3000);
+
+  }
+
+  calcular() {
+    this.total = this.data.precio * this.dias;
+  }
+
+
+  limpiar() {
+    this.usuario = '';
+    this.fecha = '';
+    this.fechaini = '';
+    this.fechafin = '';
+    this.dias = 0;
+    this.nombre = '';
+    this.correo = '';
+    this.cedula = '';
+    this.direccion = '';
+    this.nacionalidad = '';
+    this.lugardeentrega = '';
+    this.lugardedevolucion = '';
+
+  }
+
+  alertaVacio: boolean = false;
+  guardartodo() {
+    // if (this.nombre == '' || this.correo == '' || this.cedula == '' || this.telefono == '' || this.direccion == '' || this.selectedNacionalidad == '') {
+    //   this.alertaVacio = true;
+
+    //   setTimeout(() => {
+    //     this.alertaVacio = false;
+    //   }, 3000);
+
+    //   return;
+    // }
+
+    if (this.lugardeentrega == '' || this.dias == 0) {
+      this.router.navigate(['carroC']);
+      return;
     }
-  });
- }, 3000);
+    this.guardarCliente();
+    this.entrarAlquiler();
+    this.guardarPago();
+  }
+  guardarCliente() {
+    const idcliente = 0;
+    let clientetmp: cliente = new cliente();
+    clientetmp.idcliente = idcliente;
+    clientetmp.nombre = this.nombre;
+    clientetmp.correo = this.correo;
+    clientetmp.cedula = this.cedula;
+    clientetmp.telefono = this.telefono;
+    clientetmp.direccion = this.direccion;
+    clientetmp.nacionalidad = this.selectedNacionalidad;
 
-}
+    this.servicio.insertarCliente(clientetmp).subscribe((resultado: any) => {
+      if (resultado.resultado) {
+        this.idcli = resultado.idcliente;
 
-calcular() {
-  this.total = this.data.precio * this.dias;
-}
-
-
-limpiar() {
-  this.usuario = '';
-  this.fecha = '';
-  this.fechaini = '';
-  this.fechafin = '';
-  this.dias = 0;
-  this.nombre = '';
-  this.correo = '';
-  this.cedula = '';
-  this.direccion = '';
-  this.nacionalidad = '';
-  this.lugardeentrega = '';
-  this.lugardedevolucion = '';
-
-}
-
-alertaVacio:boolean = false;
-guardartodo(){
-// if (this.nombre == '' || this.correo == '' || this.cedula == '' || this.telefono == '' || this.direccion == '' || this.selectedNacionalidad == '') {
-//   this.alertaVacio = true;
-
-//   setTimeout(() => {
-//     this.alertaVacio = false;
-//   }, 3000);
-
-//   return;
-// }
-
-if( this.lugardeentrega == '' || this.dias == 0){
-  this.router.navigate(['carroC']);
-  return;
-}
-  this.guardarCliente();
-  this.entrarAlquiler();
-  this.guardarPago();
-}
-guardarCliente() {
-  const idcliente = 0;
-  let clientetmp: cliente = new cliente();
-  clientetmp.idcliente = idcliente;
-  clientetmp.nombre = this.nombre;
-  clientetmp.correo = this.correo;
-  clientetmp.cedula = this.cedula;
-  clientetmp.telefono = this.telefono;
-  clientetmp.direccion = this.direccion;
-  clientetmp.nacionalidad = this.selectedNacionalidad;
-
-  this.servicio.insertarCliente(clientetmp).subscribe((resultado: any) => {
-    if (resultado.resultado) {
-     this.idcli =  resultado.idcliente
-     
-    }
-  });
+      }
+    });
 
 
-}
-idcli:number = 0;
+  }
 
-getDescripcionTipo(tipo: number) {
-  let retorno: string = 'tipo';
-  switch (tipo) {
-    case 1:
-      retorno = 'Economico';
-      break;
+  getDescripcionTipo(tipo: number) {
+    let retorno: string = 'tipo';
+    switch (tipo) {
+      case 1:
+        retorno = 'Economico';
+        break;
 
-    case 2:
-      retorno = 'Compacto';
-      break;
+      case 2:
+        retorno = 'Compacto';
+        break;
 
-    case 3:
-      retorno = 'Deportivo';
-      break;
+      case 3:
+        retorno = 'Deportivo';
+        break;
       case 4:
         retorno = 'Premium';
         break;
@@ -255,29 +257,29 @@ getDescripcionTipo(tipo: number) {
   }
 
 
-  
 
- 
-  nationality(){
+
+
+  nationality() {
     this.servicio.nation().subscribe(response => {
-        this.nationalities = response.map(country => country.name.common);
-  this.nacionalidad = '0';
-      }, error => {
-        console.error('Error al obtener las nacionalidades:', error);
-      });
+      this.nationalities = response.map(country => country.name.common);
+      this.nacionalidad = '0';
+    }, error => {
+      console.error('Error al obtener las nacionalidades:', error);
+    });
   }
-  
- 
 
- 
 
-  
+
+
+
+
   filterNacionalidades() {
     this.filteredNacionalidades = this.nationalities.filter(nacionalidad =>
       nacionalidad.toLowerCase().includes(this.selectedNacionalidad.toLowerCase())
     );
   }
-  
+
   selectNacionalidad(nacionalidad: string) {
     this.selectedNacionalidad = nacionalidad;
     this.filteredNacionalidades = [];
@@ -295,7 +297,7 @@ getDescripcionTipo(tipo: number) {
       return;
     }
 
-    if (this.mes ==  '') {
+    if (this.mes == '') {
       this.msgFallo();
       return;
     }
@@ -304,7 +306,7 @@ getDescripcionTipo(tipo: number) {
       this.msgFallo();
       return;
     }
-    
+
     if (this.codigocv == undefined) {
       this.msgFallo();
       return;
@@ -323,21 +325,21 @@ getDescripcionTipo(tipo: number) {
     pagotmp.nombretj = this.nombretj;
     pagotmp.numerodetarjeta = this.numerodetarjeta;
     pagotmp.mes = this.mes;
-    pagotmp.ano = this.ano ;
+    pagotmp.ano = this.ano;
     pagotmp.codigocv = this.codigocv;
     pagotmp.total = this.total;
-  
+
 
     this.servicio.insertarPago(pagotmp).subscribe((resultado) => {
       if (resultado) {
         this.limpiar();
-         this.msgExicto();
-        
+        this.msgExicto();
+
       } else if (resultado == false) {
-       this.msgFallo();  
+        this.msgFallo();
       }
 
-      
+
     });
   }
 
@@ -366,5 +368,5 @@ getDescripcionTipo(tipo: number) {
 
 
 
-  
+
 
